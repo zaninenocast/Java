110750713ffb5226189d13113fa6d98a2c395003
zaninenocast <64709307+zaninenocast@users.noreply.github.com>
2020-05-16 15:52:51 +0530

    Add files via upload
    hello program

---
+++ Assignment1/Q.1.java
@@ -0,0 +1,11 @@
+package simplilearn;
+
+public class Q.1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java programming");
+
+	}
+
+}
