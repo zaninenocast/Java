f6e148ae19b3fc23dcbe6491053e1228be6a889b
zaninenocast <64709307+zaninenocast@users.noreply.github.com>
2020-05-16 15:56:06 +0530

    Add files via upload
    Subject and Marks

---
+++ Assignment1/Q2Q3.java
@@ -0,0 +1,20 @@
+package simplilearn;
+/*2.	Write a program to display the following output using a single statement.
+3.	Subject            Marks
+   Mathematics     90 
+   Computer         77 
+   Chemistry        69  
+*/
+
+public class Q2Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Subject               "+"Marks");
+		System.out.println("Mathematics           "+"90");
+		System.out.println("Computer              "+"77");
+		System.out.println("Chemistry             "+"69");
+}
+}
